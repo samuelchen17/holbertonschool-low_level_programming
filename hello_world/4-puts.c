6bac0c878e801138482a46f58bf5966187cd0a5f
Samuel Chen <sche842@gmail.com>
2026-02-12 23:33:21 +0800

    task 4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main prints "Programming is like building a multilingual puzzle
+ * return 0 (succress)
+ */
+
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
