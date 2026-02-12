b6d26ef88e292ccd731c04152e81bcb8673c7388
Samuel Chen <sche842@gmail.com>
2026-02-13 02:30:58 +0800

    0

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - print whether n is +ve or -ve
+* Return: 0
+*/
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+printf("%d is positive\n", n);
+else if (n == 0)
+printf("%d is zero\n", n);
+else
+printf("%d is negative\n", n);
+return (0);
+}
