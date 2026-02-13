03d93145cb3de48ced1c041f153837c458e26489
Samuel Chen <sche842@gmail.com>
2026-02-14 01:31:47 +0800

    task 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet in lowercase 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+print_alphabet();
+}
+}
