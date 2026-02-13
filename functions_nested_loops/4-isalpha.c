735da7cfa16e3d796d8407e3a7b12706dc80ac06
Samuel Chen <sche842@gmail.com>
2026-02-14 01:49:06 +0800

    task 4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+    * _isalpha - checks if alpha char, regardless of case
+    * @c: character (int)
+    *
+    * Return: 1 if true, 0 otherwise
+    */
+int _isalpha(int c)
+{
+if (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
