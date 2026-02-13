6d6fcbbceb4d3f57d614ad0e8608751b66e7e16d
Samuel Chen <sche842@gmail.com>
2026-02-13 10:53:46 +0800

    2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - print alphabet in lowercase
+* return: 0
+*/
+int main(void)
+{
+c = 'a';
+while (c <= 'z')
+char c;
+{
+putchar(c);
+c++;
+} 
+putchar('\n');
+return (0);
+}
