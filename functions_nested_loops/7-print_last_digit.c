0c98e4a309ef8651a5901284cb941b8c5dab1a74
Samuel Chen <sche842@gmail.com>
2026-02-14 02:37:08 +0800

    7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
-
 /**
 * _abs - print all single digit numbers of base 10
 * @n: int number
@@ -9,7 +7,12 @@
 int print_last_digit(int n)
 {
 int last_digit;
-last_digit = abs(n) % 10;
+
+if (n < 0)
+{
+n = -n;
+}
+last_digit = n % 10;
 _putchar(last_digit + '0');
 return (last_digit);
 }
