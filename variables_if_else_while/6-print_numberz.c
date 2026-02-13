0bd9c3d14ba039d74ff2981296150e54290e7077
Samuel Chen <sche842@gmail.com>
2026-02-13 11:30:22 +0800

    6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - print 0-9
+* Return: 0
+*/
+int main(void)
+{
+int n;
+n = 0;
+while (n <= 9)
+{
+/* convert to ASCII*/
+putchar(n + '0');
+n++;
+}
+printf("\n");
+return (0);
+}
