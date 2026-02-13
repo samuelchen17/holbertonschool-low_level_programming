02ac8f3721f8d29ad68475ca3de33897810f7710
Samuel Chen <sche842@gmail.com>
2026-02-13 12:19:27 +0800

    0 fix

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,10 +1,18 @@
-#include <stdio.h>
+#include <main.h>
 /**
 * main - prints "_putchar" followed by a new line
 * Return: 0
 */
 int main(void)
 {
-printf("_putchar\n");
+char s[] = "_putchar";
+int i;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
 return (0);
 }
+
