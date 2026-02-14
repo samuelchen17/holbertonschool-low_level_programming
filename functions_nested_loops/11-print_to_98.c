136834186b0c9b1512008d148ff64963266c8fb8
Samuel Chen <sche842@gmail.com>
2026-02-14 11:28:15 +0800

    11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_to_98 - print numbers from n to 98
+* @n: int number
+*/
+
+void print_to_98(int n)
+{
+int x;
+int i;
+
+x = 98;
+
+if (i <= x)
+{
+for (i = n; i <= x; i++)
+{
+if (i != n)
+{
+printf(", ")
+}
+printf("%d", i);
+}
+}
+else
+{
+for (i = n; i >= x; i--)
+{
+if (i != n)
+{
+printf(", ")
+}
+printf("%d", i);
+}
+}
+printf("\n")
+}
