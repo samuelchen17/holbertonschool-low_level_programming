913a2786165f39be45bbe28d33fc2e78436a32d9
Samuel Chen <sche842@gmail.com>
2026-02-18 20:10:48 +1100

    diag

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_diagonal - print \ * n times
+* @n: int number
+*/
+
+void print_diagonal(int n)
+{
+int i;
+int j;
+
+if (n <= 0)
+{
+_putchar('\n');
+return;
+}
+
+for (i = 0; i < n; i++)
+{
+for (j = i - 1; j > 0; j--)
+_putchar(' ')
+_putchar('\\');
+_putchar('\n');
+}
+}
