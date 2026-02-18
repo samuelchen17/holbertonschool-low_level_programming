ed38da07e17fad21f305d5156a2b4a0bbe944740
Samuel Chen <sche842@gmail.com>
2026-02-18 18:38:26 +1100

    digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _isdigit - check if c is 0-9
+* @c: int number
+* Return: 1 if true, 0 otherwise
+*/
+
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+return (0);
+}
+
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -14,5 +14,6 @@ void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
 int _isupper(int c);
+int _isdigit(int c);
 
 #endif
