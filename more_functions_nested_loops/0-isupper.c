1b3bfcb12f1c9e7147c918be7954cc7312cda838
Samuel Chen <sche842@gmail.com>
2026-02-18 18:32:59 +1100

    upper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _isupper - check if c is uppercase
+* @c: int number
+* Return: 1 if upper, 0 otherwise
+*/
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+return (0);
+}
