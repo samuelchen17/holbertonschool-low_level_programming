2459d3c38bc047aa5985d56b6d56c53655468858
Samuel Chen <sche842@gmail.com>
2026-02-18 20:28:47 +1100

    fizzbuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* fizz_buzz - print 1-100, x3 print Fizz, x5 print Buzz
+* x3 and x5 print FizzBuzz
+*/
+
+void fizz_buzz(void)
+{
+int i;
+
+for (i = 0; i <= 100; i++)
+{
+if (i % 5 == 0 && i % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz ");
+}
+else if (i % 3 == 0)
+{
+printf("FizzBuzz ");
+}
+else
+{
+printf("%d ", i);
+}
+}
+
+printf("\n");
+}
