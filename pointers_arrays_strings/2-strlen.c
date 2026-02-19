38564729e8ddf9da20f679ceb0f53302754423c0
Samuel Chen <sche842@gmail.com>
2026-02-19 21:33:07 +1100

    length

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to string
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+int counter = 0;
+
+while (*s != '\0')
+{
+counter++;
+s++;
+}
+return (counter);
+}
