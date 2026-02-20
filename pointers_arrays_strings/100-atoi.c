6bb1dd150b6064903a6ca9256c4b02554a79252f
Samuel Chen <sche842@gmail.com>
2026-02-20 19:26:09 +1100

    too many lines for poor old betty

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -17,39 +17,30 @@ int sign = 1;
 while (s[i] != '\0')
 {
 if (s[i] == '-')
-{
 minus_counter++;
-}
+
 if (s[i] >= '0' && s[i] <= '9')
-{
 break;
-}
+
 i++;
 }
 
 /* if no digits, return 0 */
 if (s[i] == '\0')
-{
 return (0);
-}
 
 /* determine the sign */
 if (minus_counter % 2 == 1)
-{
 sign = -1;
-}
 
 /* combine number */
 while (s[i] >= '0' && s[i] <= '9')
 {
 if (sign == 1)
-{
 num = num * 10 + (s[i] - '0');
-}
+
 else
-{
 num = num * 10 - (s[i] - '0');
-}
 
 i++;
 }
