77bc357bb969c7cfd1c20c061724b5dc48454b89
Samuel Chen <sche842@gmail.com>
2026-02-20 19:08:48 +1100

    atoi tested

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * _atoi - converts string to integer
+ * @s: pointer to string
+ *
+ * Return: converted integer
+ */
+
+int _atoi(char *s)
+{
+int i = 0;
+int minus_counter = 0;
+int num = 0;
+int sign = 1;
+
+while (s[i] != '\0')
+{
+if (s[i] == '-')
+{
+minus_counter++;
+}
+if (s[i] >= '0' && s[i] <= '9')
+{
+break;
+}
+i++;
+}
+
+/* if no digits, return 0 */
+if (s[i] == '\0')
+{
+return (0);
+}
+
+/* determine the sign */
+if (minus_counter % 2 == 1)
+{
+sign = -1;
+}
+
+/* combine number */
+while (s[i] >= '0' && s[i] <= '9')
+{
+num = num * 10 + (s[i] - '0');
+i++;
+}
+
+return (num * sign);
+}
