543ee79030a13f84ee46dff6128262a87ec0345e
Samuel Chen <sche842@gmail.com>
2026-02-21 19:07:29 +1100

    betty fix

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -8,15 +8,12 @@
 * Return: ascii difference of first non match char, 0 if strings match
 */
 
-
 int _strcmp(char *s1, char *s2)
 {
+int i = 0;
+
+while (s1[i] == s2[i] && s1[i] != '\0')
+i++;
+
+return (s1[i] - s2[i]);
 }
-
-
-// int i = 0
-// iterate through both strings simultaneously
-// check if characters are different
-// if different, return diff of ascii values
-// else iterate until null byte is reached
-//
