dde702d801bf9c544de0309d56b2af650490756b
Samuel Chen <sche842@gmail.com>
2026-02-21 18:24:15 +1100

    strncopy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* _strncpy - copies a string
+* @dest: destination string
+* @src: source string
+* @n: max bytes to copy from src
+*
+* Return: pointer to dest
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i = 0;
+
+/* copy src to end of dest */
+while (src[i] != '\0' && i < n)
+{
+dest[i] = src[i];
+i++;
+}
+
+/* add null byte at end */
+dest[i] = '\0';
+
+return (dest);
+}
