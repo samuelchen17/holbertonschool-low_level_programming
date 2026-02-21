36b6afa4b8dd70752438efb2efdb6eb39b536770
Samuel Chen <sche842@gmail.com>
2026-02-21 19:07:02 +1100

    strcmp

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _strcmp - compares two strings
+* @s1: first string
+* @s2: second string
+*
+* Return: ascii difference of first non match char, 0 if strings match
+*/
+
+
+int _strcmp(char *s1, char *s2)
+{
+}
+
+
+// int i = 0
+// iterate through both strings simultaneously
+// check if characters are different
+// if different, return diff of ascii values
+// else iterate until null byte is reached
+//
