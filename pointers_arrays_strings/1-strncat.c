77f99b00e748c9a4a917119e5ab46bc59ff1114c
Samuel Chen <sche842@gmail.com>
2026-02-21 17:46:47 +1100

    task 1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* _strcat - concatenates two strings
+* @dest: destination string
+* @src: source string
+* @n: max bytes to concatenate from src
+*
+* Return: pointer to dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+int i = 0, j = 0;
+
+/* find end of dest */
+while (dest[i] != '\0')
+i++;
+
+
+/* copy src to end of dest */
+while (src[j] != '\0' && j < n)
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+
+/* add null byte at end */
+dest[i] = '\0';
+
+return (dest);
+}
