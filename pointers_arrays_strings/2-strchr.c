cd9d693498a6e21305373c5a075c62acae6c8749
Samuel Chen <sche842@gmail.com>
2026-02-24 10:14:54 +1100

    edge case, check for \0

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -12,6 +12,7 @@
 char *_strchr(char *s, char c)
 {
 int i = 0;
+
 while (s[i] != '\0')
 {
 if (s[i] == c)
@@ -19,5 +20,9 @@ return (&s[i]);
 
 i++;
 }
+
+if (s[i] == c)
+return (&s[i]);
+
 return (NULL);
 }
