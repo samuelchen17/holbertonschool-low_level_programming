3c967f3d83aa1c9b16153dcb0b71ed4c5079d71e
Samuel Chen <sche842@gmail.com>
2026-02-24 10:10:06 +1100

    strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _strchr - locates a char in string
+* @s: string
+* @c: char
+*
+* Return: Returns a pointer to the first occurrence of the character c in the string s, 
+* or NULL if the character is not found
+*/
+
+char *_strchr(char *s, char c)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+if (s[i] == c)
+return (&s[i]);
+
+i++;
+}
+return (NULL);
+}
