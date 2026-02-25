5aa2d1d78ad966f6a6e50f190ee02453880fe5ff
Samuel Chen <sche842@gmail.com>
2026-02-25 22:26:11 +1100

    chessboard

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_chessboard -  print chessboard
+* @a: pointer to 2D arr of chars
+*/
+
+void print_chessboard(char (*a)[8])
+{
+int row, col;
+
+for (row = 0; row < 8; row++)
+{
+for (col = 0; col < 8; col++)
+{
+_putchar(a[row][col]);
+}
+_putchar('\n');
+}
+}
