1374d2de62a781dc148332673e894f97ac233157
Samuel Chen <sche842@gmail.com>
2026-02-25 22:13:16 +1100

    strstr

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+* _strstr -  finds first occurrence in the 'haystack'
+* of any of the bytes in 'needle'
+* @haystack: string
+* @needle: substring
+*
+* Return: Returns a pointer to the beginning of the located substring,
+* or NULL if the substring is not found.
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+int i, j;
+
+for (i = 0; haystack[i] != '\0'; i++)
+{
+for (j = 0; needle[j] != '\0'; j++)
+{
+if (haystack[i + j] != needle[j])
+{
+break;
+}
+}
+
+if (needle[j] == '\0')
+return (&haystack[i]);
+}
+
+return (NULL);
+}
