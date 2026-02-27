ef57d694638225bef3f1d1a10119ab43edf46b9d
Samuel Chen <sche842@gmail.com>
2026-02-27 21:54:56 +1100

    print arg0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * print_args - prints program name, arg[0]
+ * @argc: num of args
+ * @argv: arr of args
+ */
+
+
+void print_args(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+}
+
+/**
+ * main - prints name of program
+ * @argc: num of args
+ * @argv: arr of args
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+print_args(argc, argv);
+return (0);
+}
