2a61efc0a728705453e4edd35505d63b138edb1c
Samuel Chen <sche842@gmail.com>
2026-02-27 22:02:18 +1100

    print num of args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * print_num_of_args - prints num of args
+ * @argc: num of args
+ * @argv: arr of args
+ */
+
+
+void print_num_of_args(int argc, char *argv[])
+{
+(void)argv;
+printf("%d\n", argc);
+}
+
+/**
+ * main - call function
+ * @argc: num of args
+ * @argv: arr of args
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+print_num_of_args(argc, argv);
+return (0);
+}
