c6d1aaebbf1abf6f32a3a2721de7abda680c560e
Samuel Chen <sche842@gmail.com>
2026-02-27 22:39:40 +1100

    add args

---
+++ argc_argv/4-add.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * add_args - adds two args
+ * @argc: num of args
+ * @argv: arr of args
+ * Return: 0 if success, 1 if error
+ */
+
+int add_args(int argc, char *argv[])
+{
+int sum = 0;
+int i;
+
+/* check if num of args is correct */
+if (argc == 1)
+{
+printf("0\n");
+return (0);
+}
+
+/* check if args are digits */
+int i;
+for (i = 0; argv[1][i]; i++)
+{
+if (!isdigit(argv[1][i]))
+{
+printf("Error\n");
+return 1;
+}
+}
+
+sum = atoi(argv[1]) + atoi(argv[2]);
+printf("%d\n", sum);
+return (0);
+}
+
+/**
+ * main - call function
+ * @argc: num of args
+ * @argv: arr of args
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+return (add_args(argc, argv));
+}
