1b11c37108977ebca12e8e626757c6d3c6ee8c09
Samuel Chen <sche842@gmail.com>
2026-03-05 18:08:00 +1100

    init dog fix

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * init_dog - initialize a var of type struct dog
@@ -10,7 +11,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-if (d != NULL)
+if (d == NULL)
 return;
 
 d->name = name;
