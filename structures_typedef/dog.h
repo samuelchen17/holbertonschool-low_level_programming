60e654f10249e8e83e7ab94f369a81340ba17806
Samuel Chen <sche842@gmail.com>
2026-03-05 17:57:14 +1100

    add header guard

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,3 +1,6 @@
+#ifndef DOG_H
+#define DOG_H
+
 /**
  * struct dog - represents a dog
  * @name: dog's name
@@ -5,8 +8,11 @@
  * @owner: dog's owner
  */
 
-struct dog {
+struct dog
+{
 char *name;
 float age;
 char *owner;
 };
+
+#endif
