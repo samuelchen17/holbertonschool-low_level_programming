8b5897c5622ef53eef82a1aabb5221fef2440e76
Samuel Chen <sche842@gmail.com>
2026-03-05 18:06:03 +1100

    init dog

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize a var of type struct dog
+ * @d: pointer to struct dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d != NULL)
+return;
+
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
