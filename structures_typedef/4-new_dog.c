9d4f47793b02d65035223f366ff2619369ed0fac
Samuel Chen <sche842@gmail.com>
2026-03-06 21:55:42 +1100

    new dog

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a new dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *dog;
+
+dog = malloc(sizeof(dog_t));
+if (dog == NULL)
+return (NULL);
+
+dog->name = name;
+dog->age = age;
+dog->owner = owner;
+
+return (dog);
+}
