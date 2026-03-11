29b6f451d44bf8b63aa9a37695b57a1a7140b6d1
Samuel Chen <sche842@gmail.com>
2026-03-11 20:36:44 +1100

    main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,6 +1,37 @@
 #include "3-calc.h"
 
-int main(void) 
-{
+/**
+ * main - performs simple math operations
+ * @argc: number of args
+ * @argv: array of args
+ * Return: 0 on success else error code
+ */
 
+int main(int argc, char *argv[])
+{
+int a = atoi(argv[1]);
+int b = atoi(argv[3]);
+char op = argv[2][0];
+int op_func(int, int);
+
+if (argc != 4)
+{
+printf("Error\n");
+return (98);
+}
+
+if (op != '+' && op != '-' && op != '*' && op != '/' && op != '%')
+{
+printf("Error\n");
+return (99);
+}
+
+if (op == '/' && b == 0 || op == '%' && b == 0)
+{
+printf("Error\n");
+return (100);
+}
+
+op_func = get_op_func(argv[2]);
+printf("%d\n", op_func(a, b));
 }
