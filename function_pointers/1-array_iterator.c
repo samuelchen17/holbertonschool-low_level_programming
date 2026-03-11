3359d9c97325033b7362e42ed9202f1466034a87
Samuel Chen <sche842@gmail.com>
2026-03-11 17:10:54 +1100

    1

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iterate through an arr and exec func on each element
+ * @array: pointer to arr
+ * @size: size of arr
+ * @action: pointer to func to be called
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+size_t i;
+int *tmp;
+
+if (array == NULL || action == NULL || size == 0)
+return;
+
+tmp = array;
+
+for (i = 0; i < size; i++)
+{
+action(*tmp);
+tmp++;
+}
+}
