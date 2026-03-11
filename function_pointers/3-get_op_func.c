9e088664af349a8279d966ec7ac2bf3f9fad2cfd
Samuel Chen <sche842@gmail.com>
2026-03-11 19:49:47 +1100

    get op func complete

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,3 +1,31 @@
-#include "function_pointers.h"
+#include "3-calc.h"
+#include <stdlib.h>
 
+/**
+ * get_op_func - selects correct function given the operator
+ * @s: operator
+ * Return: ptr to correct func else NULL
+ */
 
+int (*get_op_func(char *s))(int, int)
+{
+if (s == NULL)
+return (NULL);
+
+if (*s == '+')
+return (op_add);
+
+if (*s == '-')
+return (op_sub);
+
+if (*s == '*')
+return (op_mul);
+
+if (*s == '/')
+return (op_div);
+
+if (*s == '%')
+return (op_mod);
+
+return (NULL);
+}
