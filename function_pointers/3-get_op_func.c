5d59d8454a69b5fc08128fbe9351f0eeea44ee39
Samuel Chen <sche842@gmail.com>
2026-03-11 21:26:36 +1100

    struct added and check logic changed to one if

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,15 +1,6 @@
 #include "3-calc.h"
 #include <stdlib.h>
 
-pt_t ops[] = {
-{'+', op_add},
-{'-', op_sub},
-{'*', op_mul},
-{'/', op_div},
-{'%', op_mod},
-{'\0', NULL}
-};
-
 /**
  * get_op_func - selects correct function given the operator
  * @s: operator
@@ -18,6 +9,22 @@ pt_t ops[] = {
 
 int (*get_op_func(char *s))(int, int)
 {
+static op_t ops[] = {
+{'+', op_add},
+{'-', op_sub},
+{'*', op_mul},
+{'/', op_div},
+{'%', op_mod},
+{'\0', NULL}
+};
 
+int i = 0;
+while (ops[i].op)
+{
+if (ops[i].op == s[0])
+return (ops[i].f);
+i++;
+}
 
+return (NULL);
 }
