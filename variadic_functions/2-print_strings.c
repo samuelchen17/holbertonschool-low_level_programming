68907a9044be9e86cabe7b2d08b2b726e6ea3f04
Samuel Chen <sche842@gmail.com>
2026-03-12 20:59:47 +1100

    print strings

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,39 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_strings -  print all params
+ * @separator: str to separate params
+ * @n: num of params
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+unsigned int i;
+va_list strs;
+
+va_start(strs, n);
+
+for (i = 0; i < n; i++)
+{
+
+str = va_arg(strs, char *);
+
+if (str == NULL)
+{
+printf("(nil)");
+}
+else
+{
+printf("%s", str);
+}
+
+if (i != n - 1 && separator != NULL)
+printf("%s", separator);
+}
+
+va_end(strs);
+
+printf("\n");
+}
