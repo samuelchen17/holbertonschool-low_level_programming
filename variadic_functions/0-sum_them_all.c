5cb815e3cfecd1ac0ce2416b240897f7e38ee103
Samuel Chen <sche842@gmail.com>
2026-03-12 20:27:36 +1100

    fixe mixed decl

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -10,13 +10,14 @@
 int sum_them_all(const unsigned int n, ...)
 {
 int i;
-int sum = 0;
+int sum;
 
 if (n == 0)
 return (0);
 
 va_list nums;
 va_start(nums, n);
+sum = 0;
 
 for (i = 0; i < n; i++)
 {
