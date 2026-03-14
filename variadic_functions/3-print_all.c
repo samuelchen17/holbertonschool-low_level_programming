bd5c38acf67551b5859844866807308510e94005
Samuel Chen <sche842@gmail.com>
2026-03-14 21:24:16 +1100

    print all

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,113 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_all -  print all args
+ * @format: str to specify arg types
+ */
+
+void print_all(const char * const format, ...)
+{
+int i;
+va_list args;
+void (*print_func)(va_list);
+char *separator = "";
+
+va_start(args, format);
+
+i = 0;
+
+while (format && format[i] != '\0')
+{
+print_func = get_print_func(format[i]);
+if (print_func)
+{
+printf("%s", separator);
+print_func(args);
+separator = ", ";
+}
+
+i++;
+}
+
+va_end(args);
+
+printf("\n");
+}
+
+/**
+ * get_print_func -  get print func based on type
+ * @c: char to specify type
+ * Return: ptr to func that prints based on type
+ */
+
+void (*get_print_func(char c))(va_list args)
+{
+type_t types[] = {
+{'c', print_char},
+{'i', print_int},
+{'f', print_float},
+{'s', print_string},
+{0, NULL}
+};
+
+int i = 0;
+
+while (types[i].symbol)
+{
+if (c == types[i].symbol)
+return (types[i].type);
+
+i++;
+}
+return (NULL);
+}
+
+/**
+ * print_char - print all args
+ * @args: va list
+ */
+
+void print_char(va_list args)
+{
+printf("%c", va_arg(args, int));
+}
+
+/**
+ * print_int - print all args
+ * @args: va list
+ */
+
+void print_int(va_list args)
+{
+printf("%d", va_arg(args, int));
+}
+
+/**
+ * print_float - print all args
+ * @args: va list
+ */
+
+void print_float(va_list args)
+{
+printf("%f", va_arg(args, double));
+}
+
+/**
+ * print_string - print all args
+ * @args: va list
+ */
+
+void print_string(va_list args)
+{
+char *str = va_arg(args, char *);
+if (str == NULL)
+{
+printf("(nil)");
+}
+else
+{
+printf("%s", str);
+}
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,8 +1,22 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+/**
+ * struct type - struct type
+ *
+ * @symbol: The symbol
+ * @type: ptr to function that takes a va list and prints based on type
+ */
+typedef struct type
+{
+char symbol;
+void (*type)(va_list);
+} type_t;
+
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
