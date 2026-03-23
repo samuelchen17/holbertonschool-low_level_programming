86f37f10a7a752fe0dd70bbd852bbed77bbdd0b0
Samuel Chen <sche842@gmail.com>
2026-03-23 20:54:52 +1100

    print singly linked list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_list - print all elements of a list
+ *
+ * @h: pointer to list
+ * Return: Number of characters printed
+ */
+
+size_t print_list(const list_t *h)
+{
+size_t count = 0;
+const list_t *ptr = h;
+
+while (ptr != NULL)
+{
+printf("d\n", ptr->str);
+ptr = ptr->next;
+count++;
+}
+
+return count;
+}
