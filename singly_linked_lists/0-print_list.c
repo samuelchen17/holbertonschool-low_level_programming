5221eb20274cd1952740a14dd096aad8d40be817
Samuel Chen <sche842@gmail.com>
2026-03-23 20:58:00 +1100

    betty fix

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -20,5 +20,5 @@ ptr = ptr->next;
 count++;
 }
 
-return count;
+return (count);
 }
--- singly_linked_lists/list.h
+++ singly_linked_lists/list.h
@@ -1,8 +1,6 @@
 #ifndef LIST
 #define LIST
 
-size_t print_list(const list_t *h);
-
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -13,9 +11,11 @@ size_t print_list(const list_t *h);
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
+
+size_t print_list(const list_t *h);
 
 #endif
