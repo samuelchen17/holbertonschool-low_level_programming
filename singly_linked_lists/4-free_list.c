0276c0df8235bf3a08e60fcc98e3921b92659acd
Samuel Chen <sche842@gmail.com>
2026-03-24 21:49:43 +1100

    free list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * free_list - free singularly linked list
+ *
+ * @head: ptr to head
+ */
+
+void free_list(list_t *head);
+{
+list_t *tmp;
+
+while (head != NULL)
+{
+tmp = head->next;
+free(head);
+head = tmp;
+}
+}
