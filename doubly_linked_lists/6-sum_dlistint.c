77652ea196a8e273d78383daf03f4b6fe5773a1b
Samuel Chen <sche842@gmail.com>
2026-03-26 22:28:13 +1100

    sum all data in list

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * um_dlistint - return sum of all data in list
+ *
+ * @head: ptr to current head
+ * Return: sum of all data
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+int sum = 0;
+
+if (head == NULL)
+return (0);
+
+while (head != NULL)
+{
+sum += head->n;
+head = head->next;
+}
+
+
+return (sum);
+}
