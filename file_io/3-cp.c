4b9ac9f1def09b35fb933acab004184436dd7ed1
Samuel Chen <sche842@gmail.com>
2026-03-28 01:14:47 +1100

    refactor for betty

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -19,48 +19,18 @@ exit(100);
 }
 }
 
-
 /**
- * main - copies data from src file to dst file
- *
- * @argc: arg count
- * @argv: arg vector
- *
- * Return: 0 on success, exit on failure
+ * copy_fd - copy fd from src to dst, exit on failure
+ * @fd_from: src file descriptor
+ * @fd_to: dst file descriptor
+ * @file_from: scr file
+ * @file_to: dst file
  */
 
-int main(int argc, char *argv[])
+void copy_fd(int fd_from, int fd_to, char *file_from, char *file_to)
 {
-char *file_from, *file_to;
-int fd_from, fd_to;
-ssize_t c_write, c_read;
 char buffer[1024];
-
-if (argc != 3)
-{
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-exit(97);
-}
-
-file_from = argv[1];
-file_to = argv[2];
-
-/* open src file in read only */
-fd_from = open(file_from, O_RDONLY);
-if (fd_from == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-exit(98);
-}
-
-/* open dst file for write, truncate if file exists, else create */
-fd_to = open(file_to, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-if (fd_to == -1)
-{
-close_fd(fd_from);
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-exit(99);
-}
+ssize_t c_write, c_read;
 
 /* copy from src to dst in 1024 chunks */
 while ((c_read = read(fd_from, buffer, 1024)) > 0)
@@ -82,9 +52,52 @@ close_fd(fd_from);
 close_fd(fd_to);
 exit(98);
 }
+}
+
+
+/**
+ * main - copies data from src file to dst file
+ *
+ * @argc: arg count
+ * @argv: arg vector
+ *
+ * Return: 0 on success, exit on failure
+ */
+
+int main(int argc, char *argv[])
+{
+char *file_from, *file_to;
+int fd_from, fd_to;
+ssize_t c_write, c_read;
+
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
+}
+file_from = argv[1];
+file_to = argv[2];
+
+/* open src file in read only */
+fd_from = open(file_from, O_RDONLY);
+if (fd_from == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+exit(98);
+}
+/* open dst file for write, truncate if file exists, else create */
+fd_to = open(file_to, O_WRONLY | O_TRUNC | O_CREAT,
+S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+if (fd_to == -1)
+{
+close_fd(fd_from);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+exit(99);
+}
+
+copy_fd(fd_from, fd_to, file_from, file_to);
 
 close_fd(fd_from);
 close_fd(fd_to);
-
 return (0);
 }
--- file_io/main.h
+++ file_io/main.h
@@ -7,5 +7,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void close_fd(int fd);
+void copy_fd(int fd_from, int fd_to, char *file_from, char *file_to);
 
 #endif
