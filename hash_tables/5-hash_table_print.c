58e2128476ad7dfcf69f6ee00e6dc4b2ab4a503d
Samuel Chen <sche842@gmail.com>
2026-04-01 10:43:47 +1100

    remove redundant brackets

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -1,13 +1,11 @@
 #include "hash_tables.h"
-#include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 
 /**
  * hash_table_print - prints a hash table
  * @ht: hash table
  */
 void hash_table_print(const hash_table_t *ht)
-{
 {
 unsigned long int i;
 hash_node_t *current;
@@ -35,5 +33,4 @@ current = current->next;
 }
 
 printf("}\n");
-}
 }
