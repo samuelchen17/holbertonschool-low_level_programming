a37f471b6a57fe3c9e715add84340e78c078fac6
Samuel Chen <sche842@gmail.com>
2026-03-31 19:19:34 +1100

    remove redundant variables

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -11,7 +11,6 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 hash_table_t *hashtable;
-unsigned long int i = 0;
 
 /* allocate memory for hash table */
 hashtable = malloc(sizeof(hash_table_t));
